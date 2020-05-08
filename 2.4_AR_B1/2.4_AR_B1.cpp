502dfbf988c3d814106d092a8f754fd0dc851191
Tjalling <tjallingts@gmail.com>
2020-05-08 09:46:02 +0200

    [ADD] added opencv camera code

--- 2.4_AR_B1/2.4_AR_B1.cpp
+++ 2.4_AR_B1/2.4_AR_B1.cpp
@@ -1,20 +1,56 @@
-// 2.4_AR_B1.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
 
+// shows camera vision.
+// Do not forget to add libs for this project in linker
+
+#include <opencv2/opencv.hpp>
+#include "opencv2/imgproc/imgproc.hpp" 
+#include "opencv2/highgui/highgui.hpp"
 #include <iostream>
+#include <string>
 
-int main()
+using namespace cv;
+using namespace std;
+
+int main(int argc, char* argv[])
 {
-    std::cout << "Hello World!\n";
+	// Do not forget to change camera if you have multiple
+	VideoCapture cap(0);
+
+	// Camera check
+	if (!cap.isOpened())
+	{
+		cout << "Cannot open the video cam" << endl;
+		return -1;
+	}
+
+	// Checks screen size
+	double dWidth = cap.get(CAP_PROP_FRAME_WIDTH);
+	double dHeight = cap.get(CAP_PROP_FRAME_HEIGHT);
+	cout << "Frame size : " << dWidth << " x " << dHeight << endl;
+
+	// Window frame to show live camera screen
+	namedWindow("MyVideo", WINDOW_AUTOSIZE);
+
+	// Continue loop get the frames for 
+	Mat frame;
+
+	while (1)
+	{
+		bool bSuccess = cap.read(frame);
+
+		if (!bSuccess)
+		{
+			cout << "Cannot read a frame from video stream" << endl;
+			break;
+		}
+
+		imshow("MyVideo", frame);
+
+		if (waitKey(1) == 27)
+		{
+			cout << "esc key is pressed by user" << endl;
+			break;
+		}
+	}
+	return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
